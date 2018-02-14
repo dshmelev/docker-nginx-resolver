80556441840ec80d75b4234589931ecf6fdd8e7a
Dmitry Shmelev <avikez@gmail.com>
2018-02-14 16:56:20 +0300

    [terraform] Add resolver filepath param

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,10 @@ if [ "$NAMESERVER" == "" ]; then
 	export NAMESERVER=`cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' '`
 fi
 
-echo "Nameserver is: $NAMESERVER"
-echo "resolver $NAMESERVER $RESOLVER_PARAMS;" > /etc/nginx/resolver.conf
+if ! [[ -z ${RESOLVER_FILEPATH} ]]; then
+  echo "Nameserver is: $NAMESERVER; Filepath is: $RESOLVER_FILEPATH;"
+  echo "resolver $NAMESERVER $RESOLVER_PARAMS;" > $RESOLVER_FILEPATH
+fi
 
 # default behaviour is to launch nginx
 if [[ -z ${1} ]]; then
